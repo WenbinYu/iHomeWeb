4b968db04f8bd6047a35af9ee3188ab414b36e81
wenbinyu <wenbinyu1@163.com>
2018-04-01 20:31:17 +0800

    创建项目环境\配置信息

---
+++ config.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+import redis
+
+
+class Config(object):
+    DEBUG = True
+
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome01"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置
+    REDIS_HOST = "192.168.200.128"
+    REDIS_PORT = 6379
+
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+
+
+class DevelopmentConfig(Config):
+    """创建调试环境下的配置类"""
+    pass
+
+
+class ProductionConfig(Config):
+    """创建线上环境下的配置类"""
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.72.77:3306/iHome'
+
+
+class UnittestConfig(Config):
+    """单元测试的配置"""
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome_testcast_07'
+
+
+
+configs =  {
+    'default_config':Config,
+    'development':DevelopmentConfig,
+    'production':ProductionConfig,
+    'unittest':UnittestConfig
+}
---
+++ manage.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+
+
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
+from ihome import get_app,db
+
+
+
+
+app = get_app('development')
+
+
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+
+
+if __name__ == '__main__':
+
+    print app.url_map
+
+    # app.run()
+    manager.run()
